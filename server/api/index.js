08d7afc346b675c813fd6b0e11b7523515c0f5ed
anandamayan <msanandamayan@gmail.com>
2026-03-31 20:17:05 +0530

    Modified Index on server

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const PORT = process.env.PORT || 5000;
 
 const todoRoutes = require("../routes/todos");  // ✅ go up from api/
 const authRoutes = require("../routes/auth");   // ✅ go up from api/
@@ -23,8 +24,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
-    app.listen(process.env.PORT, '0.0.0.0',() => {
-      console.log(`🚀 Server running on http://localhost:${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
